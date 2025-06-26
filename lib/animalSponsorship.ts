adbc9e84709a418bbbb13558289b33d7280f089a
szettl135 <sebastian.zettl@inode.at>
2025-06-26 14:28:49 +0200

    erster Versuch, Forntend zu verbinden

---
+++ lib/animalSponsorship.ts
@@ -0,0 +1,3 @@
+import abi from "../abi/AnimalSponsorship.json"; // von artifacts kopieren
+export const animalSponsorshipAbi = abi;
+export const animalSponsorshipAddress = "0xDEIN_DEPLOYED_ADDRESS";
