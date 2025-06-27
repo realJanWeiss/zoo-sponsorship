1116467846dae32e060e71c54e3ecee00646742c
realJanWeiss <171038268+realJanWeiss@users.noreply.github.com>
2025-06-27 10:13:26 +0200

    remove TokenStatus from the homepage but leaving the component for future use

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { fetchAnimals } from "@/api/api";
 import { Animal } from "@/types";
 import Image from "next/image";
-import TokenStatus from "./components/TokenStatus";
 
 export default async function Home() {
   const animals: Animal[] = await fetchAnimals();
@@ -9,7 +8,6 @@ export default async function Home() {
   return (
     <div>
       <h1 className="page-container">Tiergarten</h1>
-      <TokenStatus />
       <ul className="page-container grid gap-4 grid-cols-[repeat(auto-fill,_minmax(300px,_1fr))]">
         {animals.map((animal) => (
           <li key={animal.slug}>
